014fe061417ac42e69c885f46e2e9f4f45bba6cc
Daniil Ventsov <daniil.ventsov@progforce.com>
2022-08-17 09:44:40 +0300

    fix

--- API/index.ts
+++ API/index.ts
@@ -1,10 +1,10 @@
-import axios from 'axios';
+import axios from 'axios'
 // const { VUE_APP_SERVER_URL } = process.env;
 // import Cookies from 'js-cookie';
 
 export const axiosInstance = axios.create({
-  baseURL: 'http://127.0.0.1:3000',
-});
+  baseURL: 'https://pet-quasar-app.herokuapp.com/'
+})
 
 // axiosInstance.interceptors.request.use(
 //   config => {
