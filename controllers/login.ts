e2845defda93a1d150ef8924477c8e8b34a8829a
Daniil Ventsov <daniil.ventsov@progforce.com>
2022-08-17 11:08:54 +0300

    fix

--- controllers/login.ts
+++ controllers/login.ts
@@ -1,21 +1,19 @@
-import User from '../models/modelUser';
-import express from 'express';
-import createToken from '../helpers/createToken';
-const LoginUser = express.Router();
+import User from '../models/modelUser'
+import express from 'express'
+import createToken from '../helpers/createToken'
+const LoginUser = express.Router()
 
 LoginUser.post('/loginUser', async (req, res) => {
-  const { name, password } = req.body;
-  const user = await User.findOne({ $and: [{ name }, { password }] });
-
-  if (user !== null) {
+  try {
+    const { name, password } = req.body
+    const user = await User.findOne({ $and: [{ name }, { password }] })
     const token = createToken({
-      user,
-    });
-    return res.status(200).send({ massege: 'You login. Welcome', user, token });
-  } else {
-    throw new Error('Incorrect password or username');
-    return res.status(404);
+      user
+    })
+    return res.status(200).send({ massege: 'You login. Welcome', user, token })
+  } catch (error) {
+    return error.status(400)
   }
-});
+})
 
-export default LoginUser;
+export default LoginUser
