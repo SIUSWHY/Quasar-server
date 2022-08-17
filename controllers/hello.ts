1e51a6d73c774f4d615e065ef92ca1f1fea54643
Daniil Ventsov <daniil.ventsov@progforce.com>
2022-08-17 10:32:27 +0300

    fix

--- controllers/hello.ts
+++ controllers/hello.ts
@@ -1,7 +1,7 @@
 import express from 'express'
 const Hello = express.Router()
 
-Hello.get('/users', async (req, res, next) => {
+Hello.get('/hello', async (req, res, next) => {
   res.send('Hello')
 })
 
