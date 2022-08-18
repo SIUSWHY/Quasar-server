c04b03a027a1e60b311f5d5893835f62e85b9ab2
Daniil Ventsov <daniil.ventsov@progforce.com>
2022-08-18 17:40:00 +0300

    fix

--- helpers/verifyToken.ts
+++ helpers/verifyToken.ts
@@ -1,21 +1,15 @@
 import jwt from 'jsonwebtoken'
 
 function verifyToken(req: any, _res: any, next: any) {
-  // try {
+  console.log(req.headers.authorization)
+
   let token = req.headers.authorization.split(' ').pop()
 
   // verify a token symmetric - synchronous
   let decoded = jwt.verify(token, 'JWT_KEY')
 
-  console.log([req.headers.authorization, token, decoded])
-
   req.data = decoded
   next()
-  // } catch (error) {
-  //   return res.status(403).send({
-  //     errors: [error.message]
-  //   })
-  // }
 }
 
 export default verifyToken
