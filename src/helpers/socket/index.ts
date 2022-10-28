96858179997a132111967e0300560447f9ce43cf
Daniil Ventsov <daniil.ventsov@progforce.com>
2022-10-28 17:40:27 +0300

    Revert "fix"
    This reverts commit 20d56bbcbe4525124df03d054ef3488078c07364.

--- src/helpers/socket/index.ts
+++ src/helpers/socket/index.ts
@@ -15,113 +15,102 @@ function socketLogic(
   const clients = new Map<string, string>()
 
   io.on('connection', async (socket) => {
-    let token: any
-    let chatType: any
-    let user: UserType
-    let room: RoomType
+    let token: any = socket.handshake.query.token
+    let chatType: any = socket.handshake.query.chatType
+    let user = jwt.verify(token, process.env.JWT_KEY) as UserType
+    let room: RoomType = { roomId: '', chatType: '', users_id: [] }
 
-    if (socket.handshake.query.token === 'null' || undefined) {
-      socket.emit('send_room_data_to_clent', {
-        socketId: socket.id
+    clients.set(user._id, socket.id)
+
+    socket.on('get_room_id', (data) => {
+      if (!Boolean(data.roomId)) {
+        return
+      }
+
+      room.roomId = data.roomId
+    })
+
+    socket.on('get_companion_id', async (data) => {
+      if (!Boolean(room.roomId)) {
+        const roomData: RoomType = {
+          roomId: `${Date.now()}`,
+          chatType: chatType,
+          users_id: [user._id, data.companionId]
+        }
+
+        room = await modelRoom.findOne({
+          $and: [
+            { users_id: user._id },
+            { users_id: data.companionId },
+            { chatType: 'double' }
+          ]
+        })
+
+        if (!room) {
+          room = await modelRoom.create(roomData)
+        }
+      } else {
+        room = await modelRoom.findOne({ roomId: room.roomId })
+      }
+
+      socket.join(room.roomId)
+
+      const messages = await modelMessage.find({
+        roomId: room.roomId
       })
-    } else {
-      token = socket.handshake.query.token
-      chatType = socket.handshake.query.chatType
-      user = jwt.verify(token, process.env.JWT_KEY) as UserType
-      room = { roomId: '', chatType: '', users_id: [] }
-    }
 
-    // clients.set(user._id, socket.id)
+      socket.emit('send_room_data_to_clent', {
+        room: room,
+        messages: messages
+      })
+    })
 
-    // socket.on('get_room_id', (data) => {
-    //   if (!Boolean(data.roomId)) {
-    //     return
+    socket.on('save_message_to_db', async (data) => {
+      if (!room) {
+        throw new Error('NO ROOM')
+      }
+
+      saveMessageToDb(data, room)
+
+      io.to(room.roomId).emit('sent_message_to_room', { data })
+
+      room.users_id.forEach((userId) => {
+        const userIdString = userId.toString()
+        const socket_id = clients.get(userIdString)
+
+        if (userIdString === user._id) {
+          return
+        }
+
+        io.to(socket_id).emit('set_new_message_notify', room.roomId)
+      })
+    })
+
+    console.log(`
+    ${user.name} - connected`)
+
+    // socket.on('get_all_user_status', () => {
+    //   const arrUsersStatus: { userId: string; isOnline: boolean }[] = []
+    //   for (const userId of clients.keys()) {
+    //     arrUsersStatus.push({ userId: userId, isOnline: true })
     //   }
-
-    //   room.roomId = data.roomId
+    //   socket.emit('send_all_users_status', arrUsersStatus)
     // })
 
-    // socket.on('get_companion_id', async (data) => {
-    //   if (!Boolean(room.roomId)) {
-    //     const roomData: RoomType = {
-    //       roomId: `${Date.now()}`,
-    //       chatType: chatType,
-    //       users_id: [user._id, data.companionId]
-    //     }
+    sendUserStatus(true, user, clients, io)
 
-    //     room = await modelRoom.findOne({
-    //       $and: [
-    //         { users_id: user._id },
-    //         { users_id: data.companionId },
-    //         { chatType: 'double' }
-    //       ]
-    //     })
+    socket.on('disconnecting', () => {
+      sendUserStatus(false, user, clients, io)
+    })
 
-    //     if (!room) {
-    //       room = await modelRoom.create(roomData)
-    //     }
-    //   } else {
-    //     room = await modelRoom.findOne({ roomId: room.roomId })
-    //   }
+    socket.on('disconnect', () => {
+      console.log(`
+      ${user.name} - disconnected`)
+    })
 
-    //   socket.join(room.roomId)
-
-    //   const messages = await modelMessage.find({
-    //     roomId: room.roomId
-    //   })
-
-    //   socket.emit('send_room_data_to_clent', {
-    //     room: room,
-    //     messages: messages
-    //   })
-    // })
-
-    // socket.on('save_message_to_db', async (data) => {
-    //   if (!room) {
-    //     throw new Error('NO ROOM')
-    //   }
-
-    //   saveMessageToDb(data, room)
-
-    //   io.to(room.roomId).emit('sent_message_to_room', { data })
-
-    //   room.users_id.forEach((userId) => {
-    //     const userIdString = userId.toString()
-    //     const socket_id = clients.get(userIdString)
-
-    //     if (userIdString === user._id) {
-    //       return
-    //     }
-
-    //     io.to(socket_id).emit('set_new_message_notify', room.roomId)
-    //   })
-    // })
-
-    // console.log(`
-    // ${user.name} - connected`)
-
-    // // socket.on('get_all_user_status', () => {
-    // //   const arrUsersStatus: { userId: string; isOnline: boolean }[] = []
-    // //   for (const userId of clients.keys()) {
-    // //     arrUsersStatus.push({ userId: userId, isOnline: true })
-    // //   }
-    // //   socket.emit('send_all_users_status', arrUsersStatus)
-    // // })
-
-    // sendUserStatus(true, user, clients, io)
-
-    // socket.on('disconnecting', () => {
-    //   sendUserStatus(false, user, clients, io)
-    // })
-
-    // socket.on('disconnect', () => {
-    //   console.log(`
-    //   ${user.name} - disconnected`)
-    // })
-
-    // socket.on('get_data_for_group', async (data) => {
-    //   createGroupRoom(data)
-    // })
+    socket.on('get_data_for_group', async (data) => {
+      createGroupRoom(data)
+    })
   })
 }
 
