4fc1e3ba6ee8aff436f8fef34b354589dac73de1
Daniil <ventsovdaniil@yandex.ru>
2022-11-24 23:03:15 +0300

    fix

---
+++ src/helpers/storage.ts
@@ -0,0 +1,12 @@
+import dotenv from "dotenv";
+dotenv.config();
+const EasyYandexS3 = require("easy-yandex-s3");
+
+export const s3 = new EasyYandexS3({
+  auth: {
+    accessKeyId: process.env.YANDEX_STORAGE_ACESS_KEY,
+    secretAccessKey: process.env.YANDEX_STORAGE_SECRET_ACESS_KEY,
+  },
+  Bucket: "quasar-storage",
+  debug: process.env.NODE_ENV ? true : false,
+});
